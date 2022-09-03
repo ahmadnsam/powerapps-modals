7a9870c488e73275c6db1d1da78262ff22e07b8f
Ahmad Sammour <ahmadnsam@gmail.com>
2022-09-03 08:52:11 +0200

    initiate project and set the dialog content

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "./styles/pamodals.styles.vite.css";
+import ViteModal from "./components/ViteModal";
+
+function App() {
+  return (
+    <div className="App">
+      <ViteModal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ViteModal.tsx
@@ -0,0 +1,54 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+export default function ViteModal() {
+  let [isOpen, setIsOpen] = useState(true);
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="absolute z-10" onClose={closeModal}>
+        <Transition.Child as={Fragment}>
+          <div className="fixed inset-0" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex h-full items-center justify-center text-center">
+            <Transition.Child as={Fragment}>
+              <Dialog.Panel className="w-full  h-full transform overflow-hidden  bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Payment successful
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Your payment has been successfully submitted. We’ve sent you
+                    an email with all of the details of your order.
+                  </p>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    onClick={closeModal}
+                  >
+                    Got it, thanks!
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
