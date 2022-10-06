fc36313cd2fd5dd11b9133c05956edfd69bd1feb
Ahmad Sammour <ahmadnsam@gmail.com>
2022-10-06 11:38:50 +0200

    remove sample

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,6 @@ import "./styles/pamodals.styles.vite.css";
 import ViteModal from "./components/ViteModal";
 import { useEffect, useState } from "react";
 import { Modal } from "./types/Modal";
-function sample() {
-  return {
-    icon: "success",
-    labels: [
-      { text: "Published!", type: "h1" },
-      {
-        text: "Your record is published, please identify the category",
-        type: "h2",
-      },
-    ],
-    inputs: [
-      {
-        id: "choiceexample",
-        label: "Category",
-        type: "choice",
-        multi: true,
-        options: [
-          { id: "opt1", label: "Customer" },
-          { id: "opt2", label: "Prospect" },
-          { id: "opt3", label: "Partner" },
-        ],
-      },
-    ],
-    buttons: [
-      {
-        id: "button-cancel", //used to know what button was clicked, retunred with modal return object
-        label: "Cancel",
-        type: "white", //blue //red
-      },
-      {
-        id: "button-submit",
-        label: "Submit",
-        type: "blue",
-      },
-    ],
-  };
-}
 function getDataParam() {
   var vals = new Array();
   if (location.search != "") {
@@ -62,8 +25,7 @@ function App() {
     buttons: [],
   });
   useEffect(() => {
-    //setModal(getDataParam());
-    setModal(sample());
+    setModal(getDataParam());
   }, []);
   return (
     <div className="App">
--- src/components/ViteChoices.tsx
+++ src/components/ViteChoices.tsx
@@ -1,7 +1,6 @@
 import { Combobox } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
-import { ArrayHelper } from "../helpers/arrays";
 import useModalStore from "../store/modal";
 
 function classNames(...classes: (string | boolean)[]) {
@@ -10,20 +9,7 @@ function classNames(...classes: (string | boolean)[]) {
 export default function ViteChoices(props: any) {
   const { inputs, set } = useModalStore();
   const [query, setQuery] = useState("");
-  const setValue = (value: any) => {
-    if (props.multi) set(props.id, value.map(ArrayHelper.selectProps("id")));
-    else set(props.id, value.id);
-  };
-  const display = (option: any) => {
-    if (option) {
-      if (props.multi) return option.map((opt: any) => opt.label).join(",");
-      else return props.options.filter((x: any) => x.id === option)[0]?.label;
-    }
-    return "";
-  };
-  /*displayValue={(option: any) =>
-    props.options.filter((x: any) => x.id === option)[0]?.label
-  }*/
+
   const filteredOptions =
     query === ""
       ? props.options
@@ -35,8 +21,7 @@ export default function ViteChoices(props: any) {
       className="mt-2"
       as="div"
       value={inputs[props.id]}
-      multiple={props.multi ?? undefined}
-      onChange={(value: any) => setValue(value)}
+      onChange={(value: any) => set(props.id, value.id)}
     >
       <Combobox.Label className="text-left block text-sm font-medium text-gray-700">
         {props.label}
@@ -45,7 +30,9 @@ export default function ViteChoices(props: any) {
         <Combobox.Input
           className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
           onChange={(event) => setQuery(event.target.value)}
-          displayValue={(option: any) => display(option)}
+          displayValue={(option: any) =>
+            props.options.filter((x: any) => x.id === option)[0]?.label
+          }
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon
@@ -57,43 +44,40 @@ export default function ViteChoices(props: any) {
         {filteredOptions.length > 0 && (
           <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
             {filteredOptions.map((option: any) => (
-              <>
-                <span>{option.id}</span>
-                <Combobox.Option
-                  key={option.id}
-                  value={option}
-                  className={({ active }) =>
-                    classNames(
-                      "relative cursor-default select-none py-2 pl-3 pr-9",
-                      active ? "bg-indigo-600 text-white" : "text-gray-900"
-                    )
-                  }
-                >
-                  {({ active, selected }) => (
-                    <>
+              <Combobox.Option
+                key={option.id}
+                value={option}
+                className={({ active }) =>
+                  classNames(
+                    "relative cursor-default select-none py-2 pl-3 pr-9",
+                    active ? "bg-indigo-600 text-white" : "text-gray-900"
+                  )
+                }
+              >
+                {({ active, selected }) => (
+                  <>
+                    <span
+                      className={classNames(
+                        "block truncate",
+                        selected && "font-semibold"
+                      )}
+                    >
+                      {option.label}
+                    </span>
+
+                    {selected && (
                       <span
                         className={classNames(
-                          "block truncate",
-                          selected && "font-semibold"
+                          "absolute inset-y-0 right-0 flex items-center pr-4",
+                          active ? "text-white" : "text-indigo-600"
                         )}
                       >
-                        {option.label}
+                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
                       </span>
-
-                      {selected && (
-                        <span
-                          className={classNames(
-                            "absolute inset-y-0 right-0 flex items-center pr-4",
-                            active ? "text-white" : "text-indigo-600"
-                          )}
-                        >
-                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      )}
-                    </>
-                  )}
-                </Combobox.Option>
-              </>
+                    )}
+                  </>
+                )}
+              </Combobox.Option>
             ))}
           </Combobox.Options>
         )}
