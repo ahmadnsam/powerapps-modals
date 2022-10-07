2f3bc944e3f4b150b28daf00ca23ad137fd510b8
Ahmad Sammour <ahmadnsam@gmail.com>
2022-10-07 09:19:33 +0200

    remove sample data

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,7 @@ import "./styles/pamodals.styles.vite.css";
 import ViteModal from "./components/ViteModal";
 import { useEffect } from "react";
 import useModalStore from "./store/modal";
-import { Modal } from "./types/Modal";
-function sample() {
-  return {
-    icon: "success",
-    labels: [
-      { text: "Published!", type: "h1" },
-      {
-        text: "Your record is published, please identify the category",
-        type: "h2",
-      },
-    ],
-    inputs: [
-      {
-        id: "choiceexample",
-        label: "Category",
-        type: "choice",
-        multi: true,
-        required: true,
-        options: [
-          { id: "opt1", label: "Customer" },
-          { id: "opt2", label: "Prospect" },
-          { id: "opt3", label: "Partner" },
-        ],
-      },
-    ],
-    buttons: [
-      {
-        id: "button-cancel", //used to know what button was clicked, retunred with modal return object
-        label: "Cancel",
-        type: "white", //blue //red
-        cancel: true,
-      },
-      {
-        id: "button-submit",
-        label: "Submit",
-        type: "blue",
-      },
-    ],
-  };
-}
+
 function getDataParam() {
   var vals = new Array();
   if (location.search != "") {
@@ -60,8 +21,7 @@ function getDataParam() {
 function App() {
   const { modal, setModal } = useModalStore();
   useEffect(() => {
-    //setModal(getDataParam());
-    setModal(sample() as Modal);
+    setModal(getDataParam());
   }, []);
   return (
     <div className="App">
